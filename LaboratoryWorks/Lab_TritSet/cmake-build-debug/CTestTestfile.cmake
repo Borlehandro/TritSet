7e888bdb52d73f57fddaae40b6c0474ae91bcab6
Alex Borzikov <a.borzikov@g.nsu.ru>
2019-10-22 15:22:18 +0700

    ...

--- LaboratoryWorks/Lab_TritSet/cmake-build-debug/CTestTestfile.cmake
+++ LaboratoryWorks/Lab_TritSet/cmake-build-debug/CTestTestfile.cmake
@@ -4,6 +4,6 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "runAllTests")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/home/alehandro/CLionProjects/cpp_18210-18211_tasks/LaboratoryWorks/Lab_TritSet/CMakeLists.txt;21;add_test;/home/alehandro/CLionProjects/cpp_18210-18211_tasks/LaboratoryWorks/Lab_TritSet/CMakeLists.txt;0;")
+add_test(runAllTests "/home/alehandro/CLionProjects/cpp_18210-18211_tasks/LaboratoryWorks/Lab_TritSet/bin/runAllTests")
+set_tests_properties(runAllTests PROPERTIES  _BACKTRACE_TRIPLES "/home/alehandro/CLionProjects/cpp_18210-18211_tasks/LaboratoryWorks/Lab_TritSet/CMakeLists.txt;21;add_test;/home/alehandro/CLionProjects/cpp_18210-18211_tasks/LaboratoryWorks/Lab_TritSet/CMakeLists.txt;0;")
 subdirs("gtests")
