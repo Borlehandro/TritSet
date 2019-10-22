72e575543a56c5e61ffdfdb5cf2f972383e960ba
Alex Borzikov <a.borzikov@g.nsu.ru>
2019-10-22 09:16:21 +0700

    Version 1.2. Add file for all tests running and change CMake version.

--- LaboratoryWorks/Lab_TritSet/tests.cpp
+++ LaboratoryWorks/Lab_TritSet/tests.cpp
@@ -1,4 +1,7 @@
-//
-// Created by Alex Borzikov on 08.10.2019.
-//
+#include <gtest/gtest.h>
 
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
